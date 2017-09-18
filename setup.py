6f4b19cec1d62b073577b51247ecd1e618d636ae
mlelansky <mlelansky@mail.de>
2017-09-18 22:44:21 +0200

    Edit the metadata for the package.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 from distutils.util import convert_path
 import sys
 
-
 class Tox(TestCommand):
     """
     This is a special command class that runs tox from setup.py
@@ -30,7 +29,6 @@ class Tox(TestCommand):
         errno = tox.cmdline(args=args)
         sys.exit(errno)
 
-
 def get_version():
     main_ns = {}
     ver_path = convert_path('django_voldemort/version.py')
@@ -38,21 +36,34 @@ def get_version():
         exec(ver_file.read(), main_ns)
     return main_ns['__version__']
 
-
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 config = dict(
     name='django_voldemort',
-    description='',
+    description='Django Voldemort Cache Connector',
     long_description=readme(),
-    url='http://localhost.test',
+    url='http://www.project-voldemort.com',
     author='Mirko Lelansky',
     author_email='mlelansky@mail.de',
     version=get_version(),
     license='Apache',
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python 3',
+        'Programming Language :: Python 3.6',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware'
     ],
     keywords='',
     packages=['django_voldemort'],
